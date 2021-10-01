0e8535d87ce55344d21050477a792e0ef91daaab
kimkwanka <kimkwanka@gmail.com>
2021-10-01 21:02:28 +0200

    fix: enlarge nav link clickable area and make logo link to home

--- src/common/NavBar/NavBar.jsx
+++ src/common/NavBar/NavBar.jsx
@@ -4,43 +4,45 @@ import './NavBar.scss';
 
 const scrollToTarget = (e) => {
   e.preventDefault();
-  const anchorTarget = document.querySelector(e.target.hash);
+
+  const anchorTarget = document.querySelector(e.currentTarget.hash);
   anchorTarget.scrollIntoView({ behavior: 'smooth' });
 };
 
 const NavBar = () => (
   <nav className="navbar">
-    <h1 className="logo">
-      kim
-      <span className="gradient">kwanka</span>
-      .io
-    </h1>
-    <ul className="navbar__nav-items">
-      <li>
-        <a className="navbar__link" href="#home" onClick={scrollToTarget}>
-          Home
-        </a>
-      </li>
-      <li>
-        <a className="navbar__link" href="#work" onClick={scrollToTarget}>
-          Work
-        </a>
-      </li>
-      <li>
-        <a className="navbar__link" href="#about" onClick={scrollToTarget}>
-          About
-        </a>
-      </li>
-      <li>
-        <a
-          className="navbar__link button"
-          href="#contact"
-          onClick={scrollToTarget}
-        >
-          Contact
-        </a>
-      </li>
-    </ul>
+    <div className="navbar__left-side">
+      <a className="logo__link" href="#home" onClick={scrollToTarget}>
+        <h1 className="logo">
+          kim
+          <span className="gradient">kwanka</span>
+          .io
+        </h1>
+      </a>
+    </div>
+    <div className="navbar__right-side">
+      <ul className="navbar__nav-items">
+        <li>
+          <a className="navbar__link" href="#work" onClick={scrollToTarget}>
+            Work
+          </a>
+        </li>
+        <li>
+          <a className="navbar__link" href="#about" onClick={scrollToTarget}>
+            About
+          </a>
+        </li>
+        <li>
+          <a
+            className="navbar__link button"
+            href="#contact"
+            onClick={scrollToTarget}
+          >
+            Contact Me
+          </a>
+        </li>
+      </ul>
+    </div>
   </nav>
 );
 

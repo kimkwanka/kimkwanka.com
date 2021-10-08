d49e08a32540c1f8256e9879001cdc6db0c160c7
kimkwanka <kimkwanka@gmail.com>
2021-10-08 14:00:12 +0200

    refactor: cleanup components

--- src/containers/Home/ProjectCard/ProjectCard.jsx
+++ src/containers/Home/ProjectCard/ProjectCard.jsx
@@ -1,7 +1,5 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
-import React from 'react';
-
 import Link from 'next/link';
 import Image from 'next/image';
 

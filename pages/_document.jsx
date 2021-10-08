292b9c19e7b6c448bcf37174cc3537ed11d151cf
kimkwanka <kimkwanka@gmail.com>
2021-10-08 21:59:15 +0200

    fix: use unoptimized font loading snippet to leverage "Automatic Webfont Optimization"

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,12 +1,19 @@
 import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
+  Html, Head, Main, NextScript,
 } from 'next/document';
 
 // Font Loading snippet from:
 // https://csswizardry.com/2020/05/the-fastest-google-fonts/
+
+// UPDATE: Snipped removed due to the way that Next.js handles Google Webfonts
+// https://github.com/timlrx/tailwind-nextjs-starter-blog/issues/35
+// https://nextjs.org/blog/next-10-2#automatic-webfont-optimization
+
+// With the CSSWizardry snippet the font would actually
+// never load in development since onLoad="this.media='all'"
+// is removed by Next.js because of SSR and
+// (Document is only rendered in the server, event handlers like onClick won't work.)
+// https://nextjs.org/docs/advanced-features/custom-document
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -20,27 +27,9 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
-          <link
-            rel="preload"
-            as="style"
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
-          />
-          <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
-            media="print"
-            onLoad="this.media='all'"
           />
-          <noscript>
-            <link
-              rel="stylesheet"
-              href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
-            />
-          </noscript>
         </Head>
         <body>
           <Main />

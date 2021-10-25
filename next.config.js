2bb6fe54834509c2e4ee1714633a61381c777619
kimkwanka <kimkwanka@gmail.com>
2021-10-25 19:32:26 +0200

    fix: format next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
-// https://stackoverflow.com/questions/58784444/nextjs-export-is-broken-no-css-no-js
-// To enable exported out folder to work locally uncomment next line
-// assetPrefix: './',
+  // https://stackoverflow.com/questions/58784444/nextjs-export-is-broken-no-css-no-js
+  // To enable exported out folder to work locally uncomment next line
+  // assetPrefix: './',
 };

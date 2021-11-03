a6abed9efeb6a483b6cf59545cc4bcf1c4e4c1e0
Kim Kwanka <kimkwanka@gmail.com>
2021-11-03 09:38:51 +0100

    feat: start implementing animations

--- src/containers/HomeView/Hero/Hero.tsx
+++ src/containers/HomeView/Hero/Hero.tsx
@@ -11,6 +11,29 @@ import HeroBackground from './HeroBackground/HeroBackground';
 
 import styles from './Hero.module.scss';
 
+const containerVariants = {
+  initial: {},
+  animate: {
+    transition: {
+      staggerChildren: 0.1,
+      delayChildren: 0.3,
+    },
+  },
+};
+
+const itemVariants = {
+  initial: { opacity: 0, x: -100 },
+  animate: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 1,
+      // type: 'spring',
+      // bounce: 0.6,
+    },
+  },
+};
+
 const Hero = forwardRef<HTMLElement>((_, ref) => {
   const [heroText, cycleHeroText] = useCycle(
     'loves to solve problems.',
@@ -27,82 +50,95 @@ const Hero = forwardRef<HTMLElement>((_, ref) => {
   }, [heroText, cycleHeroText]);
 
   return (
-    <section className={styles.Hero} id="home" ref={ref}>
-      <HeroBackground />
-      <div className={styles.HeroContainer}>
-        <h1 className={styles.HeroTitle}>
-          <small>Hi there, my name is</small> <br /> Kim Kwanka
-        </h1>
-        <p className={styles.HeroText}>
-          I&apos;m a Berlin-based <strong>Full-Stack Web Developer</strong> that
-          specializes in React and
-          <span className={styles.HeroSpanContainer}>
-            <AnimatePresence>
-              <motion.span
-                className={styles.HeroSpan}
-                key={heroText}
-                initial={{ opacity: 0, y: -20 }}
-                exit={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{
-                  duration: 1,
-                  type: 'tween',
-                  ease: 'anticipate',
-                }}
+    <AnimatePresence>
+      <section className={styles.Hero} id="home" ref={ref}>
+        <HeroBackground />
+        <motion.div
+          className={styles.HeroContainer}
+          variants={containerVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.h1 className={styles.HeroTitle} variants={itemVariants}>
+            <small>Hi there, my name is</small> <br /> Kim Kwanka
+          </motion.h1>
+          <motion.p className={styles.HeroText} variants={itemVariants}>
+            I&apos;m a Berlin-based <strong>Full-Stack Web Developer</strong>{' '}
+            that specializes in React and
+            <span className={styles.HeroSpanContainer}>
+              <AnimatePresence>
+                <motion.span
+                  className={styles.HeroSpan}
+                  key={heroText}
+                  initial={{ opacity: 0, y: -20 }}
+                  exit={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{
+                    duration: 1,
+                    type: 'tween',
+                    ease: 'anticipate',
+                  }}
+                >
+                  {heroText}
+                </motion.span>
+              </AnimatePresence>
+            </span>
+          </motion.p>
+          <motion.div
+            className={styles.HeroButtonContainer}
+            variants={itemVariants}
+          >
+            <Link href="/#work" scroll={false}>
+              <a
+                href="#work"
+                className={`${styles.HeroButton} button`}
+                onClick={scrollToSection}
               >
-                {heroText}
-              </motion.span>
-            </AnimatePresence>
-          </span>
-        </p>
-        <div className={styles.HeroButtonContainer}>
+                Check out my{' '}
+                <span>
+                  work <span className="icon-chevron-thin-right" />
+                </span>
+              </a>
+            </Link>
+            <Link href="/files/Kim_Kwanka_Resume.pdf" scroll={false}>
+              <a
+                href="/files/Kim_Kwanka_Resume.pdf"
+                className={`${styles.HeroButtonSecondary}`}
+                target="_blank"
+                download
+              >
+                <span>
+                  <span className="icon-download" /> Download
+                </span>{' '}
+                Resume
+              </a>
+            </Link>
+          </motion.div>
+          <motion.div
+            className={styles.PortraitImageContainer}
+            variants={itemVariants}
+          >
+            <Image
+              src="/img/retro.svg"
+              alt="Kim Kwanka"
+              className={styles.PortraitImage}
+              layout="fill"
+              priority
+            />
+          </motion.div>
           <Link href="/#work" scroll={false}>
             <a
+              className={styles.DownArrow}
               href="#work"
-              className={`${styles.HeroButton} button`}
               onClick={scrollToSection}
+              aria-label="work"
             >
-              Check out my{' '}
-              <span>
-                work <span className="icon-chevron-thin-right" />
-              </span>
+              <span className="icon-arrow-down2" />
             </a>
           </Link>
-          <Link href="/files/Kim_Kwanka_Resume.pdf" scroll={false}>
-            <a
-              href="/files/Kim_Kwanka_Resume.pdf"
-              className={`${styles.HeroButtonSecondary}`}
-              target="_blank"
-              download
-            >
-              <span>
-                <span className="icon-download" /> Download
-              </span>{' '}
-              Resume
-            </a>
-          </Link>
-        </div>
-        <div className={styles.PortraitImageContainer}>
-          <Image
-            src="/img/retro.svg"
-            alt="Kim Kwanka"
-            className={styles.PortraitImage}
-            layout="fill"
-            priority
-          />
-        </div>
-        <Link href="/#work" scroll={false}>
-          <a
-            className={styles.DownArrow}
-            href="#work"
-            onClick={scrollToSection}
-            aria-label="work"
-          >
-            <span className="icon-arrow-down2" />
-          </a>
-        </Link>
-      </div>
-    </section>
+        </motion.div>
+      </section>
+    </AnimatePresence>
   );
 });
 Hero.displayName = 'About';

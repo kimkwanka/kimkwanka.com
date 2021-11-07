58d6095ebfe1d6890894839420bd20a28757678a
kimkwanka <kimkwanka@gmail.com>
2021-11-07 17:28:08 +0100

    feat: use cloudinary to serve images

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ module.exports = {
   // https://stackoverflow.com/questions/58784444/nextjs-export-is-broken-no-css-no-js
   // To enable exported out folder to work locally uncomment next line
   // assetPrefix: './',
+  images: {
+    domains: ['res.cloudinary.com'],
+    loader: 'cloudinary',
+    path: 'https://res.cloudinary.com/kimkwanka/image/upload/',
+  },
 };

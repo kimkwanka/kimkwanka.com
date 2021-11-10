d1d6d1a1203e42f345a461e627feda93a144f69e
kimkwanka <kimkwanka@gmail.com>
2021-11-10 08:12:21 +0100

    feat: re-enable next/image functions and disable cloudinary for hosting on vercel

--- next.config.js
+++ next.config.js
@@ -3,9 +3,11 @@ module.exports = {
   // https://stackoverflow.com/questions/58784444/nextjs-export-is-broken-no-css-no-js
   // To enable exported out folder to work locally uncomment next line
   // assetPrefix: './',
-  images: {
-    domains: ['res.cloudinary.com'],
-    loader: 'cloudinary',
-    path: 'https://res.cloudinary.com/kimkwanka/image/upload/',
-  },
+
+  // To use cloudinary to serve and dynamically transform the images, uncomment next block
+  // images: {
+  //   domains: ['res.cloudinary.com'],
+  //   loader: 'cloudinary',
+  //   path: 'https://res.cloudinary.com/kimkwanka/image/upload/',
+  // },
 };
